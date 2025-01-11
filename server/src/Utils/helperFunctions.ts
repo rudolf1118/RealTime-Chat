6a077603996c55298d995351ab2baa6c1711f341
Rudolf Harutyunyan <rudolf.harutyunyan@pipe17.com>
2025-01-11 15:36:52 +0400

    fix("socket's middleware")

--- server/src/Utils/helperFunctions.ts
+++ server/src/Utils/helperFunctions.ts
@@ -2,3 +2,8 @@ export const removePasswordFromUser = (user: any) => {
     const { password, ...rest } = user.toObject();
     return rest;
 }
+
+export const isValidEmail = (email: string): boolean => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
